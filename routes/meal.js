1e30d8c02cf28c2702d3bc4a191d2c75a234c3c8
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-11-15 13:38:27 +0100

    feat: Added meal controller

--- routes/meal.js
+++ routes/meal.js
@@ -4,89 +4,33 @@ const { body, param, query } = require('express-validator');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+const mealController = require('../controllers/meal_controller');
+
 // getAll /api/meal
 // add pagination
-router.get('/', async (req, res) => {
-    const meals = await prisma.meal.findMany({
-        include: {
-            dish: true
-        },
-        orderBy: {
-            id: 'desc',
-        },
-    });
-    res.json(meals);
-});
+router.get('/', mealController.getAll);
 
 // Search /api/meal/search/:name
 router.get(
     '/search/:name',
     body('name').isString(),
-    async (req, res) => {
-        const { name } = req.params;
-        const meal = await prisma.meal.findMany({
-            where: {
-                name: {
-                    contains: name
-                }
-            },
-            include: {
-                dish: true
-            },
-            orderBy: {
-                id: 'desc',
-            },
-        });
-        res.json(meal);
-    }
+    mealController.search
 );
 
 // Create /api/meal, creates a new meal that a user has eaten
-router.post('/', async (req, res) => {
-    const { userId, dishId, type } = req.body;
-
-    const meal = await prisma.meal.create({
-        data: {
-            userId: parseInt(userId),
-            dishId: parseInt(dishId),
-            type: type
-        }
-    });
-    res.json(meal);
-});
+router.post('/', mealController.create);
 
 // Update /api/meal/:id, updates a meal that a user has eaten
 router.put('/:id',
     param('id').isInt(),
-    async (req, res) => {
-        const { id } = req.params;
-        const { userId, dishId, type } = req.body;
-        const meal = await prisma.meal.update({
-            where: {
-                id: parseInt(id)
-            },
-            data: {
-                dishId: parseInt(dishId),
-                type: type
-            }
-        });
-        res.json(meal);
-    }
+    mealController.update
 );
 
 
 // Delete /api/meal/:id, deletes a meal that a user has eaten
 router.delete('/:id',
     param('id').isInt(),
-    async (req, res) => {
-        const { id } = req.params;
-        const meal = await prisma.meal.delete({
-            where: {
-                id: parseInt(id)
-            }
-        });
-        res.json(meal);
-    }
+    mealController.delete
 );
 
 module.exports = router;
