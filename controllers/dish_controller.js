3ff9d7a502752877bf66a96ca24d0c1670bfe48a
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-11-15 13:32:49 +0100

    feat: Added dish controller

---
+++ controllers/dish_controller.js
@@ -0,0 +1,73 @@
+const { validationResult } = require('express-validator');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+// getAll /api/dish
+module.exports.getAll = async (req, res) => {
+    const dishes = await prisma.dish.findMany({
+        orderBy: {
+            id: 'desc',
+        },
+    });
+    res.json(dishes);
+};
+
+// get:id /api/dish/:id
+module.exports.getOne = async (req, res) => {
+    const { id } = req.params;
+    const dish = await prisma.dish.findUnique({
+        where: {
+            id: parseInt(id)
+        },
+        // should you get the times you have eaten this here?
+    });
+    res.json(dish);
+};
+
+// Search /api/dish/search/:name
+module.exports.search = async (req, res) => {
+    const { name } = req.params;
+    const dish = await prisma.dish.findMany({
+        where: {
+            name: {
+                contains: name
+            }
+        },
+        orderBy: {
+            id: 'desc',
+        },
+        take: 10,
+    });
+    res.json(dish);
+};
+
+// Create /api/dish
+module.exports.create = async (req, res) => {
+    //const { name } = req.body;
+    const dish = await prisma.dish.create({
+        data: {
+            name: 'testBurgare11'
+        }
+    });
+    res.json(dish);
+};
+
+// delete /api/dish/:id
+module.exports.delete = async (req, res) => {
+    const { id } = req.params;
+    const dish = await prisma.dish.delete({
+        where: {
+            id: parseInt(id)
+        }
+    });
+
+    // delete all meals with this dishId
+    // Should someones past foods be deleted when I delete a dish? idk
+    // const meal = await prisma.meal.deleteMany({
+    //     where: {
+    //         dishId: parseInt(id)
+    //     }
+    // });
+
+    res.json(dish); // returns the deleted dish, kinda useless
+};
