0e24817c891aded971bd3e7bf1d13fd14d409c7c
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-11-15 13:53:10 +0100

    refactor/feat: added user controller

---
+++ controllers/user_controller.js
@@ -0,0 +1,56 @@
+const { validationResult } = require('express-validator');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+// getAll /api/user
+module.exports.getAll = async (req, res) => {
+    const users = await prisma.user.findMany({
+        orderBy: {
+            id: 'desc',
+        },
+    });
+    res.json(users);
+};
+
+// get:id /api/user/:id
+module.exports.getOne = async (req, res) => {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({
+        where: {
+            id: parseInt(id)
+        },
+    });
+    res.json(user);
+};
+
+// create /api/user, creates a new user
+module.exports.create = async (req, res) => {
+    //const { email } = req.body; // not req.body, use auth instead 
+    const user = await prisma.user.create({
+        data: {
+            email: 'testEmail14@email.com',
+        }
+    });
+    res.json(user);
+};
+
+// login? /api/user/login, logs in a user
+
+
+// delete /api/user/:id, deletes user and all meals associated with them
+module.exports.delete = async (req, res) => {
+    const { id } = req.params;
+    const meal = await prisma.meal.deleteMany({
+        where: {
+            userId: parseInt(id)
+        }
+    });
+
+    const user = await prisma.user.delete({
+        where: {
+            id: parseInt(id)
+        }
+    });
+
+    res.json(user);
+};
--- routes/user.js
+++ routes/user.js
@@ -4,62 +4,32 @@ const { body, param, query } = require('express-validator');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+const userController = require('../controllers/user_controller');
+
 // idk
 
 // getAll /api/user
-router.get('/', async (req, res) => {
-    const users = await prisma.user.findMany({
-        orderBy: {
-            id: 'desc',
-        },
-    });
-    res.json(users);
-});
+router.get('/', userController.getAll);
 
 // get:id /api/user/:id
 router.get('/:id',
     param('id').isInt(),
-    async (req, res) => {
-        const { id } = req.params;
-        const user = await prisma.user.findUnique({
-            where: {
-                id: parseInt(id)
-            },
-        });
-        res.json(user);
-    }
+    userController.getOne
 );
 
 // create /api/user, creates a new user
-router.post('/', query('email').isEmail().normalizeEmail(), async (req, res) => {
-    const { email } = req.body; // not req.body, use auth instead 
-    const user = await prisma.user.create({
-        data: {
-            email: email,
-        }
-    });
-    res.json(user);
-});
+router.post('/',
+    query('email').isEmail().normalizeEmail(),
+    userController.create
+);
 
 // login? /api/user/login, logs in a user
 
 // delete /api/user/:id, deletes user and all meals associated with them
 // tested manually and works
-router.delete('/:id', param('id').isInt(), async (req, res) => {
-    const { id } = req.params;
-    const meal = await prisma.meal.deleteMany({
-        where: {
-            userId: parseInt(id)
-        }
-    });
-
-    const user = await prisma.user.delete({
-        where: {
-            id: parseInt(id)
-        }
-    });
-
-    res.json(user);
-});
+router.delete('/:id',
+    param('id').isInt(),
+    userController.delete
+);
 
 module.exports = router;
