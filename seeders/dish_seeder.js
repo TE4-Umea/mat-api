241f7e8c5306d96a9499cdec054558c5f9679211
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-11-30 13:33:48 +0100

    refactor: Update seeder with img and desc

--- seeders/dish_seeder.js
+++ seeders/dish_seeder.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 async function main() {
     // math.random then add numbers to data?
-    const random = Math.floor(Math.random() * 10) + 1;
+    /*const random = Math.floor(Math.random() * 10) + 1;
     await prisma.dish.createMany({
         data: [
             { name: `Hamburgare${random}` },
@@ -13,6 +13,25 @@ async function main() {
             { name: `Pannkakor${random}` },
             { name: `Kebab${random}` },
             { name: `Pizza${random}` },
+        ],
+    });*/
+
+    await prisma.dish.createMany({
+        data: [
+            { name: 'Hamburgare', img: 'hamburgare.jpg', desc: 'Stek hamburgare. Servera med bröd och ketchup. ' },
+            { name: 'Cheeseburgare', img: 'cheeseburgare.jpg', desc: 'Stek hamburgare. Servera med bröd, ost och ketchup. ' },
+            { name: 'Kebab', img: 'kebab.jpg', desc: 'Stek kebab. Servera med typ pommes och någon salad. ' },
+            { name: 'Korv med bröd', img: 'korv_brod.jpg', desc: 'Stek korv. Servera med bröd och ketchup. ' },
+            { name: 'Korv och makaroner', img: 'korv_makaroner.jpeg', desc: 'Koka makaroner. Stek korv. Servera. ' },
+            { name: 'Korv stroganoff med ris', img: 'korvstroganoff_ris.jpg', desc: 'Stek korv. Blanda korv med krossade tomater och grädde. Servera med ris. ' },
+            { name: 'Falukorv med potatis', img: 'falukorv_potatis.jpg', desc: 'Stek falukorv. Koka potatis. Servera. ' },
+            { name: 'Pizza', img: 'pizza.jpg', desc: 'Ta pizzadeg och smet ut den över en plåt. Lägg på tomatsås, ost och andra ingredienser. Grädda pizzan i ugnen i 20 minuter. ' },
+            { name: 'Pannkakor', img: 'pannkakor.jpg', desc: 'Stek pannkakor. Servera med sylt och grädde. ' },
+            { name: 'Köttbullar med potatismos', img: 'kottbullar_potatismos.jpg', desc: 'Stek köttbullar. Koka potatis. Servera. ' },
+            { name: 'Köttbullar med makaroner', img: 'kottbullar_makaroner.jpg', desc: 'Stek köttbullar. Koka makaroner. Servera. ' },
+            { name: 'Makaronigryta', img: 'makaronigryta.JPG', desc: 'Koka makaroner. Stek köttfärs.  Blanda makaroner med köttfärsen. Servera. ' },
+            { name: 'Köttfärssås med spaghetti', img: 'spaghetti_kottfarssas.jpg', desc: 'Koka spaghetti. Stek köttfärs. Blanda köttfärsen med krossade tomater. Servera. ' },
+            { name: 'Skinksås med pasta', img: 'skinksas.jpg', desc: 'Koka pasta. Gör skinksås. Blanda skinkan med krossade tomater. Servera. ' },
         ],
     });
 }
